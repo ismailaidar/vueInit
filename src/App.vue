ca4f31a63a450ce796e5b358ec1070cbab8a468c
Ismail Aidar <ismail.aidar@onco360.com>
2021-04-27 17:59:16 -0400

    Add datatable

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  
+  <div class="flex flex-col h-screen justify-between">
+      <div id="nav" class="bg-red-600">
+        <Nav />
+      </div>
+      <div class="container w-full mx-auto  flex-grow">
+          <div class="w-full px-4 md:px-4 md:mt-8 text-gray-800 leading-normal bg-gray-120 h-full pt-24">
+            <router-view/>
+          </div>
+      </div>
+      <Footer />
     </div>
-    <router-view/>
-  </div>
+  
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+export default {
+  components: {
+    Nav,
+    Footer
+  }
 }
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style >
 </style>
